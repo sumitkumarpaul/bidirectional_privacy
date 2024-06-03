347e9cd32520ffff00016f0aa4d4ba7774eb5e9d
Ubuntu <sumit@SGXDC4sv3.2rgwkgtiloue5eivmj5a3n2vcf.bx.internal.cloudapp.net>
2024-06-03 23:22:43 +0000

    - Apt-get update was not working properly in the previous script. Fixed that.

--- poc/setup.sh
+++ poc/setup.sh
@@ -4,12 +4,13 @@ sudo apt-get install -y curl
 # Install Gramine
 
 sudo curl -fsSLo /usr/share/keyrings/gramine-keyring.gpg https://packages.gramineproject.io/gramine-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/gramine-keyring.gpg] https://packages.gramineproject.io/ $(lsb_release -sc) main" \
-| sudo tee /etc/apt/sources.list.d/gramine.list
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/gramine-keyring.gpg] https://packages.gramineproject.io/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/gramine.list
 sudo curl -fsSLo /usr/share/keyrings/intel-sgx-deb.asc https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/intel-sgx-deb.asc] https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -sc) main" \
-| sudo tee /etc/apt/sources.list.d/intel-sgx.list
+sudo curl -fsSLo /usr/share/keyrings/intel-sgx-deb.asc https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/intel-sgx-deb.asc] https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/intel-sgx.list
+sleep 2
 sudo apt-get update
+sleep 2
 sudo apt-get install -y gramine
 
 # Generate enclave signing-key
@@ -24,6 +25,7 @@ sudo apt install -y libsgx-dcap-quote-verify
 sudo apt remove -y libsgx-dcap-default-qpl
 # Install DCAP client
 wget https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/a/az-dcap-client/az-dcap-client_1.12.0_amd64.deb
+mkdir ~/.az-dcap-client
 sudo dpkg -i az-dcap-client_1.12.0_amd64.deb
 rm az-dcap-client_1.12.0_amd64.deb
 
@@ -31,3 +33,4 @@ rm az-dcap-client_1.12.0_amd64.deb
 sudo apt-get install -y gcc make
 sudo apt install -y pkg-config
 sudo apt install -y libxml2 libxml2-dev
+#sudo apt -y libmbedtls-dev
